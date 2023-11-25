e068034575b8c310c0699fc25dfa4d718698ca6e
Bolshoypapa <aleks13kovyazin@gmail.com>
2023-11-25 16:08:57 +0300

    Sasha

--- js/loginCheck.js
+++ js/loginCheck.js
@@ -1,26 +1,29 @@
-document.getElementById("loginForm").addEventListener("submit", function(event) {
-  event.preventDefault();
-
+function authenticate() {
   // Получаем значения логина и пароля из формы
-  var username = document.getElementById("username").value;
-  var password = document.getElementById("password").value;
+  var username = document.getElementById('username').value;
+  var password = document.getElementById('password').value;
 
-  // Загружаем содержимое файла vhod.txt с GitHub
+  // Получаем содержимое файла vhod.txt из GitHub
   fetch('https://github.com/Bolshoypapa/Simple_Kovyazin/blob/main/vhod.txt')
       .then(response => response.text())
       .then(data => {
           // Разбиваем содержимое файла на строки
           var lines = data.split('\n');
 
-          // Проверяем, совпадает ли введенный логин и пароль с содержимым файла
-          if (username === lines[0].trim() && password === lines[1].trim()) {
-              alert("Вы авторизовались");
-              // Здесь можно добавить код для перенаправления на защищенную страницу
+          // Получаем логин и пароль из файла
+          var correctUsername = lines[1].trim();
+          var correctPassword = lines[2].trim();
+
+          // Проверяем соответствие введенных данных данным из файла
+          if (username === correctUsername && password === correctPassword) {
+              alert('Авторизация успешна!');
+              // Здесь можно выполнить дополнительные действия после успешной авторизации
           } else {
-              alert("Вы не авторизовались");
+              alert('Неверный логин или пароль. Попробуйте еще раз.');
           }
       })
       .catch(error => {
-          console.error('Error fetching vhod.txt:', error);
+          console.error('Ошибка при получении файла: ', error);
+          alert('Произошла ошибка. Пожалуйста, попробуйте позже.');
       });
-});
+}
