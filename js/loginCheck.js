26015517f6557d6ec6cd7b390a6c546c437e0e98
Bolshoypapa <aleks13kovyazin@gmail.com>
2023-11-25 15:57:39 +0300

    Sasha

--- js/loginCheck.js
+++ js/loginCheck.js
@@ -1,29 +1,26 @@
-function authenticate() {
-  // Получаем значения логина и пароля из формы
-  var username = document.getElementById('username').value;
-  var password = document.getElementById('password').value;
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+  event.preventDefault();
 
-  // Получаем содержимое файла vhod.txt из GitHub
+  // Получаем значения логина и пароля из формы
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+
+  // Загружаем содержимое файла vhod.txt с GitHub
   fetch('https://github.com/Bolshoypapa/Simple_Kovyazin/blob/main/vhod.txt')
       .then(response => response.text())
       .then(data => {
           // Разбиваем содержимое файла на строки
           var lines = data.split('\n');
 
-          // Получаем логин и пароль из файла
-          var correctUsername = lines[1].trim();
-          var correctPassword = lines[2].trim();
-
-          // Проверяем соответствие введенных данных данным из файла
-          if (username === correctUsername && password === correctPassword) {
-              alert('Авторизация успешна!');
-              // Здесь можно выполнить дополнительные действия после успешной авторизации
+          // Проверяем, совпадает ли введенный логин и пароль с содержимым файла
+          if (username === lines[0].trim() && password === lines[1].trim()) {
+              alert("Вы авторизовались");
+              // Здесь можно добавить код для перенаправления на защищенную страницу
           } else {
-              alert('Неверный логин или пароль. Попробуйте еще раз.');
+              alert("Вы не авторизовались");
           }
       })
       .catch(error => {
-          console.error('Ошибка при получении файла: ', error);
-          alert('Произошла ошибка. Пожалуйста, попробуйте позже.');
+          console.error('Error fetching vhod.txt:', error);
       });
-}
+});
